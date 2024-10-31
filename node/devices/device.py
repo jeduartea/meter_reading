958333368530414846c5311deaf21d9b03118286
jeduartea <67802211+jeduartea@users.noreply.github.com>
2024-10-31 17:07:17 -0500

    Update device.py

--- node/devices/device.py
+++ node/devices/device.py
@@ -68,6 +68,7 @@ class Device:
 
 # Prueba de conexión al ejecutar el script de forma individual
 if __name__ == "__main__":
+    import os
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
     if parent_dir not in sys.path:
