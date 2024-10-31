c6a1141050c9d9c7e95bd15324711907f04f83ec
jeduartea <67802211+jeduartea@users.noreply.github.com>
2024-10-31 15:48:26 -0500

    Update varible.py

--- node/varibles/varible.py
+++ node/varibles/varible.py
@@ -13,7 +13,7 @@ class Varible:
     
     def get_last_vaule(self, chip) -> None:
 
-        serial_port = serial.Serial(port="/dev/ttyS1", baudrate=230400, timeout=0.1)
+        serial_port = serial.Serial(port="/dev/ttyS1", baudrate=230400, timeout=1)
         msg = f'{{"chip" : "{chip}", "operation" : "{self.code}"}}'
         print(f"Enviando mensaje: {msg}")
         serial_port.write(msg.encode())
